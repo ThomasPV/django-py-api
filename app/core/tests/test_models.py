ff02462e3c0168387feccc3342c848107770fb19
Thomas <ptvx001@gmail.com>
2019-05-03 10:20:31 +0530

    Added User SuperUser

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from django.core.exceptions import ValidationError
-
 class ModelsTest(TestCase):
 
     def test_create_user_email(self):
